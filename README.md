406e8913101d6ec87683c221e8df3630017fb756
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-02-12 16:38:40 +0100

    music search api

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # AJAX-music-search
+
+### Overview
+Project url(https://rawaldeep.github.io/AJAX-music-search/)
+
+### Objectives
+There's **two main user stories** to this app:
+
+* A user can search for artists by name and they are displayed as a list so the user can see the search result
+* A user can click a search result item and it displays a list of related youtube videos for the user to watch/listen
+
+
+### Search with autocomplete
+
+
+![search-feature](https://i.gyazo.com/c082c4ba5eb929a45ead06890554a24c.gif)
+
+#### Select Artist
+Upon selecting an artist out of the suggestion list, display a list of videos from the selected artist
+
+
+![search-feature](https://i.gyazo.com/476d5da034d8b3ce8b108e9e3a2dae07.gif)
+
+
+#### How to get started
+
+Search any artist name and if it is in the api list you will be able to see their name and by clicking on their name you will be able to watch their youtube videos.
+
+### Completion checklist
+
+- [x] Create an autocomplete search with your AJAX knowledge
+- [x] Display the autocomplete options in a clickable list
+- [x] Allow the user to select an artist
+- [x] Fetch and display the available youtube videos for the selected artist
+- [x] Optional: using the YouTube player API display the lyrics of the song that is playing
+
---
+++ js/script.js
@@ -0,0 +1,94 @@
+const getYoutube = (artistId, artistName) => {
+    let list = document.querySelector('.results');
+    list.innerHTML = '';
+    document.querySelector(`.search`).value = '';
+    let artistTitle = document.querySelector('.artist');
+    artistTitle.innerHTML = artistName;
+    const url = `https://musicdemons.com/api/v1/artist/${artistId}/songs`;
+    let youtubeId = [];
+     fetch(url)
+    .then(function(response) {return response.json()})
+    .then(function(myJson) {
+        for(let i in myJson){
+            youtubeId.push(myJson[i].youtube_id);
+        }
+    let videos = document.querySelector('.songs');
+    if(videos.getElementsByTagName('li').length >= 1){
+        videos.innerHTML = "";
+        }
+    youtubeId.forEach(element => {
+        li = document.createElement("li");
+        video = document.createElement("iframe");
+        video.setAttribute('class' , 'videos');
+        video.setAttribute('src' , `https://www.youtube.com/embed/${element}`);
+        video.setAttribute('frameborder' , `0`);
+        video.setAttribute('picture-in-picture', true);
+        video.setAttribute('allow', 'accelerometer');
+        video.setAttribute('encrypted-media', true);
+        video.setAttribute('gyroscope', true);
+        video.setAttribute('allowfullscreen', true);
+        li.appendChild(video);
+        videos.appendChild(li); 
+    });
+    // console.log(youtubeId)
+
+    });
+    
+}
+    
+    const createResultList = (results) => {
+        // let list1 = document.querySelector('.artist');
+        let list = document.querySelector('.results');
+        let li;
+        const handleKeyDownSearch = (y) =>{
+            if(!document.querySelector(`.search`).value){
+            if(y.getElementsByTagName('li').length >= 1){
+            y.innerHTML = "";
+            // x.innerHTML = '';
+            }
+        }
+        }
+        // list1.innerHTML = '';
+        list.innerHTML = '';
+        for(let i in results){
+            
+            li = document.createElement("li");
+            li.appendChild(document.createTextNode(`${results[i].name}`));
+            li.setAttribute("id", results[i].id); // added line
+            li.setAttribute('onclick',`getYoutube(${results[i].id}, '${results[i].name}')`);
+            list.appendChild(li); 
+        
+       }
+       document.querySelector(`.search`).addEventListener(`keydown`, handleKeyDownSearch(list));
+     
+    }
+
+    const search = value => {
+
+        const url = `http://musicdemons.com/api/v1/artist/autocomplete`;
+        const options = {
+            method: 'POST',
+            headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: `name=${value}`
+            };
+         fetch(url,options)
+        .then(function(response) {return response.json()})
+        .then(function(myJson) {
+            createResultList(myJson);
+        });
+
+    };
+
+    const handleKeyUpSearch = e => {
+        const $input = e.currentTarget;
+        search($input.value);
+
+    };
+
+    const init = () => {
+        document.querySelector(`.search`).addEventListener(`keyup`, handleKeyUpSearch);
+    };
+
+    init();
